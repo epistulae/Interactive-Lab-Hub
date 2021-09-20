9bd847a067d3cb18a8562d46677de77a0b4da9c6
Cynthia <st638@cornell.edu>
2021-09-20 17:59:50 -0400

    Lyrics clock, started

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -60,17 +60,136 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+# Lab 2 Clock Formatting function
+def set_next_x(x, string):
+    x += font.getsize(string)[0]
+    return x
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     # Lab 2 Clock
     # Read current time.
-    current_time = time.strftime("%m/%d/%Y %H:%M:%S")
+    cur_date = time.strftime("%m/%d/%Y")
+    cur_hour = time.strftime("%H")
+    cur_min_sec = time.strftime("%M:%S")
 
-    # Print time
+    # Print day
     y = top
-    draw.text((x,y), current_time, font=font, fill="#FFFFFF")
+    x = font.getsize(" ")[0]*11
+    grey = "#e8e8e8"
+    draw.text((x,y), cur_date, font=font, fill=grey)
+    line_inc = font.getsize(cur_date)[1]
+    y += line_inc*2
+
+    #test
+    cur_hour = "5"
+    # Musical Lyrics: A new lyric for every hour of the day
+    x = 0
+
+    # Style by Taylor Swift
+    if cur_hour == "0":
+      draw.text((x,y), "Midnight,", font=font, fill="#8ca7ff")
+      x = set_next_x(x, "Midnight,")
+      draw.text((x,y), " you come and", font=font, fill=grey)
+      x = 0
+      y += line_inc
+      draw.text((x,y), "pick me up, no headlights", font=font, fill=grey)
+
+    # 1 AM by Beautiful Creatures
+    elif cur_hour == "1":
+      draw.text((x,y), "1 A.M.", font=font, fill="#8241f2")
+      x = set_next_x(x, "1 A.M.")
+      draw.text((x,y), " and I'm cold again", font=font, fill=grey)
+      x = 0
+      y += line_inc
+      draw.text((x,y), "I'm alone again and", font=font, fill=grey)
+      y += line_inc
+      draw.text((x,y), "I need a friend", font=font, fill=grey)
+
+    # 2:AM by Anthony Russo
+    elif cur_hour == "2":
+      draw.text((x,y), "And now it's", font=font, fill=grey)
+      x = set_next_x(x, "And now it's")
+      draw.text((x,y), " 2 A.M.", font=font, fill="#8835cc")
+      x = 0
+      y += line_inc
+      draw.text((x,y), "I'm up, all in my head", font=font, fill=grey)
+
+    # Heartless (Remix) by Kanye West
+    elif cur_hour == "3":
+      draw.text((x,y), "Why we up ", font=font, fill=grey)
+      x = set_next_x(x, "Why we up ")
+      draw.text((x,y), " 3 A.M.", font=font, fill="#bd34d9")
+      x = set_next_x(x, " 3 A.M.")
+      draw.text((x,y), " on the phone?", font=font, fill=grey)
+      x = 0
+      y += line_inc
+      draw.text((x,y), "Why do she be so mad at me for?", font=font, fill=grey)
+
+    # 4 AM and Nothing New by Sugarplum Fairy
+    elif cur_hour == "4":
+      draw.text((x,y), "4 A.M.", font=font, fill="#eb3dd1")
+      x = set_next_x(x, "4 A.M.")
+      draw.text((x,y), " and nothing new", font=font, fill=grey)
+      x = 0
+      y += line_inc
+      draw.text((x,y), "You wear your shiny", font=font, fill=grey)
+      y += line_inc
+      draw.text((x,y), "Sunday shoes", font=font, fill=grey)
+
+    # Before It's Light by Novi Novak
+    elif cur_hour == "5":
+      draw.text((0,y), "I think about it every night", font=font, fill=grey)
+      y += line_inc
+      draw.text((x,y), "when I'm up til", font=font, fill=grey)
+      x = set_next_x(x, "when I'm up til")
+      draw.text((x,y), " Five AM", font=font, fill="#f53699")
+      y += line_inc
+      draw.text((0,y), "Good life out of sight", font=font, fill=grey)
+
+    # Mafia by SCH
+    elif cur_hour == "6":
+      x = 0
+    elif cur_hour == "7":
+      x = 0
+    elif cur_hour == "8":
+      x = 0
+    elif cur_hour == "9":
+      x = 0
+    elif cur_hour == "10":
+      x = 0
+    elif cur_hour == "11":
+      x = 0
+    elif cur_hour == "12":
+      x = 0
+    elif cur_hour == "13":
+      x = 0
+    elif cur_hour == "14":
+      x = 0
+    elif cur_hour == "15":
+      x = 0
+    elif cur_hour == "16":
+      x = 0
+    elif cur_hour == "17":
+      x = 0
+    elif cur_hour == "18":
+      x = 0
+    elif cur_hour == "19":
+      x = 0
+    elif cur_hour == "20":
+      x = 0
+    elif cur_hour == "21":
+      x = 0
+    elif cur_hour == "22":
+      x = 0
+    elif cur_hour == "23":
+      x = 0
+
+    x = font.getsize(" ")[0]*15
+    y += line_inc*2
+    draw.text((x,y), cur_min_sec, font=font, fill=grey)
 
     # Display image.
     disp.image(image, rotation)
