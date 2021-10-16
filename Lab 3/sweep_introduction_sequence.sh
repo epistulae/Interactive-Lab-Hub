853c55cebf917d2078dda8d2e9117e9e2519326d
Cynthia <st638@cornell.edu>
2021-10-16 03:54:37 -0400

    interaction shells

---
+++ Lab 3/sweep_introduction_sequence.sh
@@ -0,0 +1,8 @@
+#https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
+#!/bin/bash
+
+./sweep_intro.sh
+
+arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+command=$(python3 sweep_responses.py recorded_mono.wav)
+echo $command
