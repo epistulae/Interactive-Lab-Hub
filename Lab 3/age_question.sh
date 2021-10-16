a734d20b2996653b8c13bad614822a1478a3e31a
Cynthia <st638@cornell.edu>
2021-10-16 02:03:24 -0400

    speech to text

---
+++ Lab 3/age_question.sh
@@ -0,0 +1,9 @@
+#https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
+
+#!/bin/bash
+say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+#say $*
+say "How old are you? State each digit separately. For example, if you're twenty one, say two one."
+
+arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+python3 test_words.py recorded_mono.wav
