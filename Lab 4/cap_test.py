c82511507a38215e5b934ccecf8479ba3689c6d3
Cynthia <st638@cornell.edu>
2021-10-24 19:02:51 -0400

    playing around with threads and multiprocessing

--- Lab 4/cap_test.py
+++ Lab 4/cap_test.py
@@ -1,7 +1,7 @@
 import time
 import board
 import busio
-import os
+import multiprocessing, signal
 
 import adafruit_mpr121
 
@@ -9,10 +9,24 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
+
+def play_music(name):
+    while !interrupt_thread:
+        print(f"music thread" + name)
+        time.sleep(10)
+        print(f"thread complete")
+
+processes = list()
+
 while True:
     for i in range(12):
         if mpr121[i].value:
             print(f"Twizzler {i} touched!")
-        if mpr121[1].value:
-            os.system("mpg123 rex-incognito.wav")
-    time.sleep(1)  # Small delay to keep from spamming output messages.
+    if mpr121[1].value:
+        music = threading.Thread(target=play_music, args=("song",))
+        threads.append(music)
+        music.start()
+        print(f"Thread started")
+    if mpr121[9].value:
+        interrupt_thread = true
+    time.sleep(0.5)  # Small delay to keep from spamming output messages.
