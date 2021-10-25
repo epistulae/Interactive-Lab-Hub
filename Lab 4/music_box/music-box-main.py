e1b1b1724ce7e8650d7dad428df20177640fe3f0
Cynthia Siyi Tu <st638@cornell.edu>
2021-10-24 21:31:58 -0400

    Update music-box-main.py

--- Lab 4/music_box/music-box-main.py
+++ Lab 4/music_box/music-box-main.py
@@ -14,30 +14,30 @@ mpr121 = adafruit_mpr121.MPR121(i2c)
 
 # Global process variable control.
 manager = multiprocessing.Manager()
-Global = manager.Namespace()
-Global.music_process_id = 0
+processes = manager.Namespace()
+processes.music_process_id = 0
 
 def play_music(song_name):
     print(f"music thread " + song_name)
-    Global.music_process_id = os.getpid()
-    print(Global.music_process_id)
-    Global.music_process_id = subproccess.run(["aplay", "music_files/" + song_name], capture_output=True, shell=True)
-    print(Global.music_process_id)
+    processes.music_process_id = os.getpid()
+    print(processes.music_process_id)
+    processes.music_process_id = subprocess.run(["aplay", "music_files/" + song_name], capture_output=True, shell=True)
+    print(processes.music_process_id)
 
 while True:
     for i in range(12):
         if mpr121[i].value:
             print(f"Twizzler {i} touched!")
     if mpr121[7].value:
-        Global.music_process_id = subproccess.run(["aplay", "music_files/rex-incognito.wav"], capture_output=True, shell=True)
+        Global.music_process_id = subprocess.run(["aplay", "music_files/rex-incognito.wav"], capture_output=True, shell=True)
         print(f"Thread started rex")
     if mpr121[9].value:
         music = multiprocessing.Process(target=play_music, args=("let-the-living-beware.wav",))
         music.start()
         print(f"Thread started living")
     if mpr121[11].value:
-        print(Global.music_process_id)
-        if (Global.music_process_id is not 0):
+        print(processes.music_process_id)
+        if (processes.music_process_id is not 0):
             print(f"hello")
         else:
             print(f"there")
