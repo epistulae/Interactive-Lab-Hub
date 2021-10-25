9dd5330f2bb329cc2df7d8ce0d29987753b67b3a
Cynthia Siyi Tu <st638@cornell.edu>
2021-10-24 21:54:50 -0400

    Update music-box-main.py

--- Lab 4/music_box/music-box-main.py
+++ Lab 4/music_box/music-box-main.py
@@ -32,8 +32,8 @@ while True:
         Global.music_process_id = subprocess.run(["aplay music_files/rex-incognito.wav &"], capture_output=True, shell=True)
         print(f"Thread started rex")
     if mpr121[9].value:
-        music = multiprocessing.Process(target=play_music, args=("let-the-living-beware.wav",))
-        music.start()
+        #music = multiprocessing.Process(target=play_music, args=("let-the-living-beware.wav",))
+        play_music("let-the-living-beware.wav")
         print(f"Thread started living")
     if mpr121[11].value:
         print(Process.music_process_id)
