bdccf028d61f6d127987952f7325d6f57f503374
Cynthia Siyi Tu <st638@cornell.edu>
2021-10-24 22:53:04 -0400

    Update music-box-main.py

--- Lab 4/music_box/music-box-main.py
+++ Lab 4/music_box/music-box-main.py
@@ -20,10 +20,10 @@ Process.music_process = 0
 def play_music(song_name):
     print(f"music thread " + song_name)
     # Process.music_process_id = os.getpid()
-    music = subprocess.Popen(["aplay music_files/let-the-living-beware.wav & echo \"$!\""], shell=True)
-    Process.music_process = music.pid
+    music = subprocess.Popen(["aplay music_files/let-the-living-beware.wav & echo \"$!\""], stdout=Process.music_process, shell=True)
     print("pid " + str(music.pid) + "\n")
-    print("stdout " +music.stdout)
+    print("stdout " + music.stdout)
+    print("stdout already in? " + Process.music_process)
 
 while True:
     for i in range(12):
