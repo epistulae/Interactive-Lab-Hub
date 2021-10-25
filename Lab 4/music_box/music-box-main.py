a9dfac84a5ef9a26ac1d36d804cb4a504c604bfe
Cynthia Siyi Tu <st638@cornell.edu>
2021-10-24 21:05:26 -0400

    Update music-box-main.py

--- Lab 4/music_box/music-box-main.py
+++ Lab 4/music_box/music-box-main.py
@@ -10,28 +10,32 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
+# Global process variable control.
+manager = multiprocessing.Manager()
+Global = manager.Namespace()
+Global.music_process_id = 0
 
-def play_music(name):
-    print(f"music thread" + name)
-    process_id = os.getpid()
+def play_music(song_name):
+    print(f"music thread " + song_name)
+    Global.music_process_id = os.getpid()
     print(process_id)
-    os.system ('aplay music_files/rex-incognito.wav')
-
-global process_id
-process_id = 0
+    os.system ("aplay music_files/" + song_name)
 
 while True:
     for i in range(12):
         if mpr121[i].value:
             print(f"Twizzler {i} touched!")
-    if mpr121[9].value:
-        music = multiprocessing.Process(target=play_music, args=("cynthia",))
+    if mpr121[7].value:
+        music = multiprocessing.Process(target=play_music, args=("rex-incognito.wav",))
         music.start()
-        print(f"Thread started")
+        print(f"Thread started rex")
+    if mpr121[9].value:
+        music = multiprocessing.Process(target=play_music, args=("let-the-living-beware.wav",))
+        music.start()
+        print(f"Thread started living")
     if mpr121[11].value:
-        if (process_id is not 0):
+        if (Global.music_process_id is not 0):
             print(f"hello")
-            os.system ('aplay music_files/let-the-living-beware.wav')
             os.kill(process_id, signal.SIGINT)
             processes[0].terminate()
         else:
