228cf6d7e18e637d998d80aa5c53da43180ff0dd
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-05 01:01:11 -0400

    Update capacity-display.py

--- Lab 5/zenith/capacity-display.py
+++ Lab 5/zenith/capacity-display.py
@@ -57,7 +57,6 @@ tracking_file = open("tracking.txt","r")
 
 tracking_day = int(tracking_file.readline())
 habit_a = tracking_file.readline().split(" ")
-empty = tracking_file.readline()
 habit_b = tracking_file.readline().split(" ")
 
 tracking_file.close()
