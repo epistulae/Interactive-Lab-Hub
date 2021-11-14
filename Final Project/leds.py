72a53599f6bd7b5e00d79048347c0d8190407724
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-14 15:18:59 -0500

    constellation star values

--- Final Project/leds.py
+++ Final Project/leds.py
@@ -30,19 +30,12 @@ class Star:
         self.index = index
         self.complete = complete
         self.next_star = None
-        self.final = False
         # Previous stars: (star, connector)
         self.prior_stars = []
 
-pinpricks = [0, 58, 59, 60] 
+pinpricks = [0, 58, 59, 60, 76, 77, 98, 99, 100, 101, 122, 123, 124, 125, 130, 142, 143] 
 
-# Constellation Trees
-narwhale = Star(0)
-serpens = Star(0)
-draco = Star(0)
-shield = Star(0)
-
-# Hourglass
+# Constellation Graphs
 hourglass = Star(1)
 
 hourglass_2 = Star(6)
@@ -58,13 +51,14 @@ hourglass_4 = Star(14)
 hourglass_3.next_star = hourglass_4
 hourglass_4.prior_stars.append((hourglass_3, [11, 12, 13]))
 
-hourglass_5 = Star(19)
+hourglass_final = Star(19)
 hourglass_5.final = True
 hourglass_4.next_star = hourglass_5
 hourglass_5.prior_stars.append((hourglass_3, [20, 21, 22]))
 hourglass_5.prior_stars.append((hourglass_4, [15, 16, 17, 18]))
 
-# Teapot
+# -------------------------------------------------------
+
 teapot = Star(50)
 
 teapot_2 = Star(46)
@@ -93,21 +87,189 @@ teapot_7 = Star(37)
 teapot_6.next_star = teapot_7
 teapot_7.prior_stars.append((teapot_6, [36]))
 
-teapot_8 = Star(57)
-teapot_8.final = True
-teapot_7.next_star = teapot_8
-teapot_8.prior_stars.append((teapot, [51, 52, 53, 54, 55, 56]))
-teapot_8.prior_stars.append((teapot_5, [39, 40]))
-teapot_8.prior_stars.append((teapot_7, [38]))
+teapot_final = Star(57)
+teapot_final.final = True
+teapot_7.next_star = teapot_final
+teapot_final.prior_stars.append((teapot, [51, 52, 53, 54, 55, 56]))
+teapot_final.prior_stars.append((teapot_5, [39, 40]))
+teapot_final.prior_stars.append((teapot_7, [38]))
 
+# -------------------------------------------------------
 
+triangle = Star(65)
 
-triangle = Star(0)
-orion = Star(0)
-butterfly = Star(0)
+triangle_2 = Star(61)
+triangle.next_star = triangle_2
+triangle_2.prior_stars.append((triangle, [62, 63]))
+
+triangle_3 = Star(68)
+triangle_3.final = True
+triangle_2.next_star = triangle_3
+triangle_3.prior_stars.append((triangle, [66, 67]))
+triangle_3.prior_stars.append((triangle_2, [69, 70, 71]))
+
+# -------------------------------------------------------
+
+butterfly = Star(72)
+
+butterfly_2 = Star(97)
+butterfly.next_star = butterfly_2
+butterfly_2.prior_stars.append((butterfly, [73]))
+
+butterfly_3 = Star(75)
+butterfly_2.next_star = butterfly_3
+butterfly_3.prior_stars.append((butterfly_2, [74]))
+
+butterfly_4 = Star(93)
+butterfly_3.next_star = butterfly_4
+butterfly_4.prior_stars.append((butterfly_2, [94, 95, 96]))
+butterfly_4.prior_stars.append((butterfly_3, [78, 79, 80]))
+
+butterfly_5 = Star(87)
+butterfly_4.next_star = butterfly_5
+butterfly_5.prior_stars.append((butterfly_4, [81, 82, 83, 84, 85, 86]))
+
+butterfly_6 = Star(88)
+butterfly_6.final = True
+butterfly_5.next_star = butterfly_6
+butterfly_6.prior_stars.append((butterfly_4, [89, 90, 91, 92]))
+
+# -------------------------------------------------------
+
+orion = Star(102)
+
+orion_2 = Star(105)
+orion.next_star = orion_2
+orion_2.prior_stars.append((orion, [103, 104]))
+
+orion_3 = Star(108)
+orion_2.next_star = orion_3
+orion_3.prior_stars.append((orion_2, [106, 107]))
+
+orion_4 = Star(109)
+orion_3.next_star = orion_4
+
+orion_5 = Star(112)
+orion_4.next_star = orion_5
+orion_5.prior_stars.append((orion, [110, 111]))
+
+orion_6 = Star(114)
+orion_5.next_star = orion_6
+orion_6.prior_stars.append((orion_5, [113]))
+
+orion_7 = Star(116)
+orion_6.next_star = orion_7
+orion_7.prior_stars.append((orion_6, [115]))
+
+orion_8 = Star(118)
+orion_8.final = True
+orion_7.next_star = orion_8
+orion_8.prior_stars.append((orion_7, [117]))
+orion_8.prior_stars.append((orion_3, [119, 120, 121]))
+
+# -------------------------------------------------------
+
+serpens = Star(40)
+
+serpens_2 = Star(37)
+serpens.next_star = serpens_2
+serpens_2.prior_stars.append((serpens, [138, 139]))
+
+serpens_3 = Star(35)
+serpens_2.next_star = serpens_3
+serpens_3.prior_stars.append((serpens, [141]))
+serpens_3.prior_stars.append((serpens_2, [136]))
+
+serpens_4 = Star(33)
+serpens_3.next_star = serpens_4
+serpens_4.prior_stars.append((serpens_3, [127, 128]))
+
+serpens_5 = Star(29)
+serpens_4.next_star = serpens_5
+serpens_5.prior_stars.append((serpens_4, [131, 132]))
+
+serpens_6 = Star(26)
+serpens_6.final = True
+serpens_5.next_star = serpens_6
+serpens_6.prior_stars.append((serpens_5, [127, 128]))
+
+# -------------------------------------------------------
+
+narwhale = Star(0)
+
+narwhale_2 = Star(0)
+narwhale.next_star = narwhale_2
+
+narwhale_3 = Star(0)
+narwhale_2.next_star = narwhale_3
+
+narwhale_4 = Star(0)
+narwhale_3.next_star = narwhale_4
+
+narwhale_5 = Star(0)
+narwhale_4.next_star = narwhale_5
+
+narwhale_6 = Star(0)
+narwhale_6.final = True
+narwhale_5.next_star = narwhale_6
+
+# -------------------------------------------------------
+
+draco = Star(0)
+
+draco_2 = Star(0)
+draco.next_star = draco_2
+
+draco_3 = Star(0)
+draco_2.next_star = draco_3
+
+draco_4 = Star(0)
+draco_3.next_star = draco_4
+
+draco_5 = Star(0)
+draco_4.next_star = draco_5
+
+draco_6 = Star(0)
+draco_5.next_star = draco_6
+
+draco_7 = Star(0)
+draco_6.next_star = draco_7
+
+draco_8 = Star(0)
+draco_7.next_star = draco_8
+
+draco_9 = Star(0)
+draco_8.next_star = draco_9
+
+draco_10 = Star(0)
+draco_9.next_star = draco_10
+
+draco_11 = Star(0)
+draco_10.next_star = draco_11
+
+draco_12 = Star(0)
+draco_11.next_star = draco_12
+
+draco_13 = Star(144)
+draco_13.final = True
+draco_12.next_star = draco_13
+draco_13.prior_stars.append((draco_12, [145]))
+
+# -------------------------------------------------------
+
+shield = Star(0)
+
+shield_2 = Star(0)
+
+shield_3 = Star(0)
+
+shield_4 = Star(0)
+
+shield_5 = Star(0)
+shield_5.final = True
 
 # Habit Lists in fill order
-habit_a = [narwhale, serpens, draco, shield]
+habit_a = [serpens] # narwhale, serpens, draco, shield
 habit_b = [hourglass, teapot, triangle, orion, butterfly]
 
 # Show state of one constellation (habit)
