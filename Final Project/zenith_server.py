07fe6aff95416641990f4b986c2042b8d59dd991
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-15 20:33:55 -0500

    remove migrated functions and imports

--- Final Project/zenith_server.py
+++ Final Project/zenith_server.py
@@ -4,7 +4,6 @@ import adafruit_mpr121
 import argparse
 import board
 import busio
-from enum import Enum
 import led_controls as Leds
 from rpi_ws281x import *
 import time
@@ -37,67 +36,6 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 STRIP = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 STRIP.begin()
 STATE = State()
-
-class Colors(Enum):
-    INCOMPLETE = Color(237, 108, 2)
-    COMPLETE = Color(20, 164, 217)
-    PINPRICK = Color(255, 245, 222)
-    RED = Color(235, 52, 52)
-
-def updateStar(strip, star):
-    led_color = Colors.COMPLETE.value if star.complete else Colors.INCOMPLETE.value
-    # Star
-    strip.setPixelColor(star.index, led_color)
-    update = star.complete 
-    # Connectors
-    for prior in star.prior_stars:
-        if update:
-            led_color = Colors.COMPLETE.value if prior[0].complete else Colors.INCOMPLETE.value
-            leds = prior[1]
-            for led in leds:
-                strip.setPixelColor(led, led_color)
-    strip.show()
-
-# Show state of habits
-def displayHabits(strip):
-    for led in Stars.PINPRICKS:
-        strip.setPixelColor(led, Colors.PINPRICK.value)
-
-    star_count = 1
-    # Habit A
-    for constellation in Stars.HABIT_A.constellations:
-        for star in constellation:
-#             print("displayed star: " + str(star_count))
-#             star_count += 1
-            led_color = Colors.COMPLETE.value if star.complete else Colors.INCOMPLETE.value
-            # Star
-            strip.setPixelColor(star.index, led_color)
-            update = star.complete 
-            # Connectors
-            for prior in star.prior_stars:
-                if update:
-                    led_color = Colors.COMPLETE.value if prior[0].complete else Colors.INCOMPLETE.value
-                leds = prior[1]
-                for led in leds:
-                    strip.setPixelColor(led, led_color)
-
-    # Habit B
-    for constellation in Stars.HABIT_B.constellations:
-        for star in constellation:
-#             print("displayed star: " + str(star_count))
-#             star_count += 1
-            led_color = Colors.COMPLETE.value if star.complete else Colors.INCOMPLETE.value
-            # Star
-            strip.setPixelColor(star.index, led_color)
-            update = star.complete 
-            # Connectors
-            for prior in star.prior_stars:
-                if update:
-                    led_color = Colors.COMPLETE.value if prior[0].complete else Colors.INCOMPLETE.value
-                leds = prior[1]
-                for led in leds:
-                    strip.setPixelColor(led, led_color)
-    strip.show()
 
 def displayMode(strip):
     print("Show display mood")
@@ -140,76 +78,7 @@ def nextDay():
         else:
             # Next star
             Stars.HABIT_B.cur_star += 1
-    
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
 
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
-
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
-
-def rainbow(strip, wait_ms=20, iterations=1):
-    """Draw rainbow that fades across all pixels at once."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((i+j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-# def testing(strip):
-#     strip.setPixelColor(0, BACKGROUND)
-#     strip.setPixelColor(1, COMPLETE)
-#     strip.setPixelColor(2, COMPLETE)
-#     strip.setPixelColor(3, COMPLETE)
-#     strip.setPixelColor(4, COMPLETE)
-#     strip.setPixelColor(5, COMPLETE)
-#     strip.setPixelColor(6, COMPLETE)
-#     for i in range(7, 25):
-#         strip.setPixelColor(i, INCOMPLETE)
-#     strip.show()
-        
-# def rainbowCycle(strip, wait_ms=20, iterations=5):
-#     """Draw rainbow that uniformly distributes itself across all pixels."""
-#     for j in range(256*iterations):
-#         for i in range(strip.numPixels()):
-#             strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-#         strip.show()
-#         time.sleep(wait_ms/1000.0)
-        
-
-# def theaterChaseRainbow(strip, wait_ms=50):
-#     """Rainbow movie theater light style chaser animation."""
-#     for j in range(256):
-#         for q in range(3):
-#             for i in range(0, strip.numPixels(), 3):
-#                 strip.setPixelColor(i+q, wheel((i+j) % 255))
-#             strip.show()
-#             time.sleep(wait_ms/1000.0)
-#             for i in range(0, strip.numPixels(), 3):
-#                 strip.setPixelColor(i+q, 0)
 def debugHabits():
     print("========================================")
     print("Habits debugging")
@@ -230,8 +99,10 @@ def debugHabits():
             count_s += 1
         count_c += 1
 
-# the # wildcard means we subscribe to all subtopics of IDD
 topic = 'Colors/#'
+topic_color = 'Colors/'
+topic_mode = 'Mode/'
+topic_lights = 'Lights/'
 
 def on_connect(client, userdata, flags, rc):
 	print("connected with result code " + str(rc))
@@ -251,7 +122,6 @@ def on_message(client, userdata, msg):
 
 	# if msg.topic == 'IDD/some/other/topic': do thing
 
-
 # Every client needs a random ID
 client = mqtt.Client(str(uuid.uuid1()))
 # configure network encryption etc
@@ -270,11 +140,6 @@ def subscribing():
 sub = multiprocessing.Process(target=subscribing, args=())
 sub.start()
 
-# Process arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-args = parser.parse_args()
-
  # Setup
 STRIP = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 STRIP.begin()
@@ -282,12 +147,8 @@ STRIP.begin()
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
-print ('Press Ctrl-C to quit.')
-if not args.clear:
-    print('Use "-c" argument to clear LEDs on exit')
-
 # Leds
-displayHabits(STRIP)
+Leds.displayHabits(STRIP)
 
 try:
     while True:
@@ -335,5 +196,4 @@ try:
         
 except KeyboardInterrupt:
     sub.terminate()
-    if args.clear:
-        Leds.colorWipe(STRIP, Color(0,0,0), 10)
+    Leds.colorWipe(STRIP, Color(0,0,0), 10)
