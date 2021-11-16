5f76293a1cb28139bb476476b65b66aa53bed039
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-16 14:23:01 -0500

    Create globals.py

---
+++ Final Project/globals.py
@@ -0,0 +1,13 @@
+import habit_controls as Habits
+import led_controls as Leds
+
+HOST = '100.64.1.201'
+PORT = 7827
+USER = 'cynthia'
+PASS = 'RoomOfRequirement'
+
+DEBUG = False
+STRIP = None
+
+leds = Leds.leds
+habits = Habits.habits
