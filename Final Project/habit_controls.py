209bd859fd8654d1244a8f36399364927714e4db
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-16 17:58:48 -0500

    Update habit_controls.py

--- Final Project/habit_controls.py
+++ Final Project/habit_controls.py
@@ -19,10 +19,8 @@ def flipFirstHabit(strip, habits, leds, debug=False):
     constellation = habits.first.cur_constellation
     star = habits.first.cur_star
     habits.first.constellations[constellation][star].complete = not habits.first.constellations[constellation][star].complete
-    if leds.lights and (leds.mode is 0):
-        # Already on habits
-        Leds.updateStar(strip, habits.first.constellations[constellation][star])
-    else:
+    if not leds.lights and not (leds.mode is 0):
+        # Not on and not on habits
         leds.lights = True
         leds.mode = 0
         Leds.displayHabits(strip, habits)
@@ -35,10 +33,8 @@ def flipSecondHabit(strip, habits, leds, debug=False):
     constellation = habits.second.cur_constellation
     star = habits.second.cur_star
     habits.second.constellations[constellation][star].complete = not habits.second.constellations[constellation][star].complete
-    if leds.lights and (leds.mode is 0):
-        # Already on habits
-        Leds.updateStar(strip, habits.first.constellations[constellation][star])
-    else:
+    if not leds.lights and not (leds.mode is 0):
+        # Not on and not on habits
         leds.lights = True
         leds.mode = 0
         Leds.displayHabits(strip, habits)
