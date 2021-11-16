4e7672b8f06be09c8e2cc0ce87e24179c52acf50
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-15 21:19:33 -0500

    remove debugging

--- Final Project/habit_controls.py
+++ Final Project/habit_controls.py
@@ -28,8 +28,7 @@ def flipSecondHabit(strip):
 
 def nextDay():
     day = time.localtime()[2]
-    print(str(day))
-    day = int(input("day: "))
+
     if day is not habits.day:
         habits.day = day
         # Habit A
