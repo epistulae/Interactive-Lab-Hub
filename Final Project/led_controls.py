f53fe9c8b2ef57d6b41533f2b251544b1d4d5195
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-20 09:11:17 -0500

    rm stock animations

--- Final Project/led_controls.py
+++ Final Project/led_controls.py
@@ -126,35 +126,6 @@ def solidColor(strip, leds):
         strip.setPixelColor(i, color)
     strip.show()
 
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
-
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
-
 def rainbow(strip, leds, wait_ms=20):
     # Currently changing across all pixels, change this to per constellation
     while (leds.mode is 2) and (not leds.intercept):
