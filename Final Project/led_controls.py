31e0b6dff85b290f9bcbf489396d5f5491f74094
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-20 09:21:58 -0500

    wheel needed for rainbow

--- Final Project/led_controls.py
+++ Final Project/led_controls.py
@@ -127,6 +127,17 @@ def solidColor(strip, leds):
         strip.setPixelColor(i, color)
     strip.show()
 
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
 def rainbow(strip, leds, wait_ms=20):
     # Currently changing across all pixels, change this to per constellation
     while (leds.mode is 2) and (not leds.intercept):
