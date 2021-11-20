5c5882f451967f66e2f319020fc9a0aeac43ffe6
Cynthia Siyi Tu <st638@cornell.edu>
2021-11-20 09:32:04 -0500

    understand wheel

--- Final Project/led_controls.py
+++ Final Project/led_controls.py
@@ -129,6 +129,7 @@ def solidColor(strip, leds):
 
 def wheel(pos):
     """Generate rainbow colors across 0-255 positions."""
+    # Moving colors 3 steps at a time, one value constant, others moving in opposite directions from 0-255 and 255-0
     if pos < 85:
         return Color(pos * 3, 255 - pos * 3, 0)
     elif pos < 170:
